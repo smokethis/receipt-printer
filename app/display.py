b65cb59d5663a27c36fe86b69c63b22c320d1d85
smokethis <64678625+smokethis@users.noreply.github.com>
2026-02-28 15:37:22 +0000

    Try rotating display.

--- app/display.py
+++ app/display.py
@@ -5,7 +5,7 @@ DISPLAY_HEIGHT = 320
 
 def write_to_display(img):
     """Rotate and write a landscape PIL image to the portrait framebuffer"""
-    rotated = img.rotate(90, expand=True)
+    rotated = img.rotate(270, expand=True)
     pixels = np.frombuffer(rotated.tobytes(), dtype=np.uint8)
     r = pixels[0::3].astype(np.uint16)
     g = pixels[1::3].astype(np.uint16)
